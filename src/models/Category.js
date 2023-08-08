bf5804366f7c2f2b2de73002c0c92dadedcb04a8
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-08-08 20:36:28 -0300

    feat: implements category model
    requirement 7

---
+++ src/models/Category.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require('sequelize')
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+const CategorySchema = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  })
+
+  return CategoryTable;
+};
+
+module.exports = CategorySchema;
